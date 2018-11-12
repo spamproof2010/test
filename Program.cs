56acdc0c50720b30be09f40078909ef4e0facd15
unknown <reynald.adolphe@gmail.com>
2018-11-11 19:45:54 -0800

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_commits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
