328ec6e5dbc3471bd7b16e7185827af55b1c74a0
unknown <reynald.adolphe@gmail.com>
2018-11-11 19:48:32 -0800

    Made 1st change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace test_commits
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Made 1st change for a commit!");
         }
     }
 }
